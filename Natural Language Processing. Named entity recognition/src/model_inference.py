625e8e111bbbc1472c8db10f42078041a310b708
Sasha Lytvak <120226308+sashaLitv@users.noreply.github.com>
2026-08-15 14:49:47 +0300

    Task 1: Fix model_inference.py and add CLI support

--- Natural Language Processing. Named entity recognition/src/model_inference.py
+++ Natural Language Processing. Named entity recognition/src/model_inference.py
@@ -12,6 +12,9 @@ from transformers import AutoTokenizer, TFAutoModelForTokenClassification
 import numpy as np
 from functools import singledispatchmethod
 from IPython.display import display, HTML
+import sys
+import argparse
+from IPython import get_ipython
 
 class MountainDetector:
     '''A class to encapsulate the NER model and tokenizer for detecting mountain names'''
@@ -94,42 +97,110 @@ class MountainDetector:
     def _(self, texts: list):
         return [self._predict(t) for t in texts]
 
+    def _is_notebook(self) -> bool:
+        try:
+            shell = get_ipython().__class__.__name__
+            if shell == 'ZMQInteractiveShell':
+                return True   ## jupyter notebook or qtconsole
+            elif shell == 'TerminalInteractiveShell':
+                return False  ## terminal running IPython
+            else:
+                return False  ## other type 
+        except NameError:
+            return False      ## probably standard Python interpreter
+
+
     def visualize(self, text: str, predictions: list):
         '''
             Renders text with colored highlights for detected entities.
+            Automatically detects if running in Jupyter (HTML) or CLI (ANSI colors).
             Args:
                text(str): the original input sentence.
                predictions(list): the list of tuple (words, tags)
         '''
-        html_out = f"<div style='font-size: 16px; line-height: 1.6; padding: 10px; border: 1px solid #ddd; border-radius: 5px; margin-bottom: 10px;'>"
-        html_out += f"<div style='margin-bottom: 8px; font-weight: bold;'>Input: {text}</div>"
-        
-        for word, tag in predictions:
-            if tag == "O":
-                html_out += f"<span>{word} </span>"
-            else:
-                ## If is mountain (B-MOUNTAIN або I-MOUNTAIN), highlight
-                color = "#00ff886a" 
-                html_out += f"<span style='background-color: {color}; padding: 2px 5px; border-radius: 4px; font-weight: 500; margin-right: 4px;'>{word}</span>"
-                
-        html_out += "</div>"
-        display(HTML(html_out))
+        is_jupyter = self._is_notebook()
+
+        if is_jupyter:
+            html_out = f"<div style='font-size: 16px; line-height: 1.6; padding: 10px; border: 1px solid #ddd; border-radius: 5px; margin-bottom: 10px;'>"
+            html_out += f"<div style='margin-bottom: 8px; font-weight: bold;'>Input: {text}</div>"
+            
+            for word, tag in predictions:
+                if tag == "O":
+                    html_out += f"<span>{word} </span>"
+                else:
+                    ## If is mountain (B-MOUNTAIN або I-MOUNTAIN), highlight
+                    color = "#00ff886a" 
+                    html_out += f"<span style='background-color: {color}; padding: 2px 5px; border-radius: 4px; font-weight: 500; margin-right: 4px;'>{word}</span>"
+                    
+            html_out += "</div>"
+            display(HTML(html_out))
+
+        else:
+            cli_out = f"Input: {text}\nOutput: "
+            for word, tag in predictions:
+                if tag == "O":
+                    cli_out += f"{word} "
+                else:
+                    # \033[1m - bold, \033[92m - green, \033[0m - reset
+                    cli_out += f"\033[1m\033[92m{word}\033[0m "
+            
+            print(cli_out + "\n")
+
+def main(model_dir: str, text: str = None, file_path: str = None):
+    '''
+        The function for running inferrnce.
+        Accepts the model directory and either a single text string or a path to a file.
+    '''
+    if not text and not file_path:
+        print("Error: You must provide either --text or --file")
+        sys.exit(1)
+    elif text and file_path:
+        print("Error: Please provide either --text OR --file, not both")
+        sys.exit(1)
+
+    try:
+        detector = MountainDetector(model_dir)
+        test_texts = []
+        if file_path:
+            path_obj = Path(file_path)
+            if not path_obj.exists():
+                raise FileNotFoundError(f"Input file not found: {path_obj}")
+            with open(path_obj, "r", encoding="utf-8") as f:
+                test_texts = [line.strip() for line in f if line.strip()]
+        else:
+            test_texts = [text]
+
+        predictions = detector.predict(test_texts)
+
+        for origin, prediction in zip(test_texts, predictions):
+            detector.visualize(origin, prediction)
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
-    MODEL_PATH = "deberta_ner_model"
-    try:
-        detector = MountainDetector(MODEL_PATH)
-        test_texts = [
-            "We are planning to climb Mount Everest and Kilimanjaro next year",
-            "The Andes is the longest continental mountain range in the world",
-            "I went to the supermarket to buy some apples",
-            "He named his dog Everest",
-            "Kyiv is the capital of Ukraine"
-        ]
-        print(detector.predict(test_texts))
+    parser = argparse.ArgumentParser(description="Run inference using the trained NER Mountain Detector")
+    parser.add_argument(
+        "--model_dir", 
+        type=str, 
+        required=True, 
+        help="Path to the directory containing the fine-tuned model"
+    )
+    parser.add_argument(
+        "--text", 
+        type=str, 
+        required=False,
+        help="A single text string to process"
+    )
+    parser.add_argument(
+        "--file", 
+        type=str, 
+        required=False,
+        help="Path to a text file containing sentences to process (one sentence per line)."
+    )
 
-    except FileNotFoundError as e:
-        print(f"Error:{e}")
+    args = parser.parse_args()
+    main(model_dir=args.model_dir, text=args.text, file_path=args.file)
 
         
         
